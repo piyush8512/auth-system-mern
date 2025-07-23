b5fe5bd5242ba5f5aa42ce0939238d464d2db02f
piyush8512 <prajapatipiyush851@gmail.com>
2025-07-23 10:22:13 +0530

    first commit

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 # auth-system-mern
 =======
 >>>>>>> 98071c95e1c1ad5b052860557f84e1e4e5b51ef1
+# auth-system-mern
--- backend/src/utils/constant.js
+++
@@ -1,8 +0,0 @@
-export const ALLOWED_GENRES = [
-    'Fiction', 'Non-Fiction', 'Science Fiction', 'Fantasy', 'Mystery',
-  'Thriller', 'Romance', 'Horror', 'Biography', 'History',
-  'Self-Help', 'Childrens', 'Young Adult', 'Poetry', 'Travel',
-  'Cooking', 'Art', 'Business', 'Technology', 'Science',
-  'Health', 'Education', 'Religion', 'Spirituality', 'Comics',
-  'Graphic Novels', 'Manga', 'True Crime', 'Classics', 'Contemporary'
-]
